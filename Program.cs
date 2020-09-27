f6a216a179740b9c7b6e6a240589bb6747872f36
nezih <nezihkasim@gmail.com>
2020-09-27 05:12:34 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace project2_nezih
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int k = 0;  // holds the number of rovers
+            char direction_inp;  // to get the initial direction of the rover
+              
+            Console.WriteLine("Type the map size as two integers with a space between them (X,Y):"); // we're typing the map SIZE and lower-left corner is (0,0) 
+            var plateauSize = Console.ReadLine().Trim().Split(' ').Select(int.Parse).ToList();
+            int[,] roverCoordinates = new int[plateauSize[0], plateauSize[1]];  // Represents the plateau. Used to store the rover locations.  (1st D: x coordinates, 2nd D: y coordinates)
+            //var plateauSize = Convert.plateauSize.Select(t => Regex.Replace(t, @"\s+", "")).ToList();  // TRIED TO REMOVE WHITE SPACES WITH REGEX
+            //Console.WriteLine("x: {0}  y: {1}", plateauSize[0], plateauSize[1]);     //  TEST LINE
+
+            Console.WriteLine("\nPress 'Enter' to start:");
+
+            while (Console.ReadKey().Key != ConsoleKey.Escape)
+            {
+                Console.WriteLine("\nType the initial location of the {0}th rover as two integers (X,Y) and direction (N,E,S,W):", k + 1);
+                var initialLocation = Console.ReadLine().ToUpper().Trim().Split(' '); //  !!!!!  INPUT IS GıVEN WITH SPACES   !!!!  TRIED TO REMOVE THE WHITESPACES WITH REGEX, BUT FAILED :(
+                direction_inp = char.ToUpper(char.Parse(initialLocation[2])); 
+
+                Console.WriteLine("\nType the commands (L,R,M): ");
+                string inputs = Console.ReadLine().ToUpper();   // reading orders from user and converting all of the characters to upper-case
+
+                Rover rover = new Rover(k+1, int.Parse(initialLocation[0]), int.Parse(initialLocation[1]), inputs, direction_inp); // defining a new rover with ID, coordinates, orders, and direction
+
+                rover.Action(plateauSize, inputs, roverCoordinates); // map size, orders, and rover locations are given to Action method
+                if (rover.out_bound == 1) // checking the boundary flag
+                {
+                    continue;  // not to increment the paramater k and not to save the wrong coordinates
+                }
+                Console.WriteLine("\nThe last location of the {0}th rover after the move: ", k + 1);
+                //Console.WriteLine($"{rover.location_x} {rover.location_y} {rover.direction}");
+                rover.Info_rover(); // the method writing the ID, the coordinates, and the direction of the rover
+                roverCoordinates[rover.location_x, rover.location_y] = 1;  // putting 1 where the rover stops to check later.
+                k++;
+
+                Console.WriteLine("Press 'ESC' button to quit: ");
+            }
+            
+
+            Console.WriteLine("\n\n\n PProgram is closing...");  // why doesn't show the first 'P'?
+            Thread.Sleep(3000);
+            
+        }
+    }
+}
---
+++ Rover.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace project2_nezih
+{
+    class Rover
+    {
+        public int id { get; set; }
+        public int location_x { get; set; }
+        public int location_y { get; set; }
+        public string orders { get; set; }
+        //public string [] direction = { "W", "N", "E", "S" };  // 0=West  1=North   2=East   3=South
+        public char direction { get; set; }  // 0=West  1=North   2=East   3=South
+        public int out_bound { get; set; }   // flag to check boundary conditions
+
+        public Rover(int _id, int _location_x, int _location_y, string _orders, char _direction)
+        {
+            this.id = _id;
+            this.location_x = _location_x;
+            this.location_y = _location_y;
+            this.orders = _orders;
+            this.direction = _direction;
+        }
+
+        public void Info_rover()
+        {
+            Console.WriteLine($"Rover id: {this.id}   (X,Y) = ({this.location_x}, {this.location_y}),   Direction: {this.direction}\n");
+        }
+
+
+        public void Action(List<int> plateauSize, string inputs, int[,] roverLocations)
+        {
+            int old_location_x;  // to reassign the older values of the rover when it faces with an error
+            int old_location_y;
+            char old_direction;
+
+            old_location_x = this.location_x;
+            old_location_y = this.location_y;
+            old_direction = this.direction;
+
+            int i = 0;           
+
+            while (i < inputs.Length)    // processes for each order character
+            {
+                switch (inputs[i])
+                {
+                    case 'M':
+                        if ((this.direction == 'N' && this.location_y + 1 < plateauSize[1]) || (this.direction == 'E' && this.location_x + 1 < plateauSize[0]) || (this.direction == 'S' && this.location_y - 1 >= 0) || (this.direction == 'W' && this.location_x - 1 >= 0))  // boundary check line
+                        {
+                            if (this.direction == 'N') { this.location_y += 1; }        // when boundary conditions meet, the rover moves
+                            else if (this.direction == 'E') { this.location_x += 1; } 
+                            else if (this.direction == 'S') { this.location_y -= 1; } 
+                            else if (this.direction == 'W') { this.location_x -= 1; }
+
+                            if (roverLocations[this.location_x, this.location_y] == 1)  // when the rover faces with another rover, it gives collision error, and gets back to the initial coordinates
+                            {
+                                Console.WriteLine($"\nCollision with another rover at ({this.location_x} , {this.location_y}). Please try a different command.\n");
+                                this.location_x = old_location_x;
+                                this.location_y = old_location_y;
+                                this.direction = old_direction;
+                                out_bound = 1;  // boundary flag is used to notify the error
+                                break;
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine($"\nOut of bounds at ({this.location_x} , {this.location_y}). Please try a different command.\n");
+                            this.location_x = old_location_x;
+                            this.location_y = old_location_y;
+                            this.direction = old_direction;
+                            out_bound = 1;
+                            break;
+                        }
+
+                        if (roverLocations[this.location_x, this.location_y] == 1)  // when the rover faces with another rover, it gives collision error, and gets back to the initial coordinates
+                        {
+                            Console.WriteLine($"\nCollision with another rover at ({this.location_x} , {this.location_y}). Please try a different command.\n");
+                            this.location_x = old_location_x;
+                            this.location_y = old_location_y;
+                            this.direction = old_direction;
+                            out_bound = 1;  // boundary flag is used to notify the error
+                            break;
+                        }
+                                                
+                        break;
+
+                    case 'L':
+                        if (this.direction == 'N') { this.direction = 'W'; }
+                        else if (this.direction == 'E') { this.direction = 'N'; }
+                        else if (this.direction == 'S') { this.direction = 'E'; }
+                        else if (this.direction == 'W') { this.direction = 'S'; }
+                        break;
+
+                    case 'R':
+                        if (this.direction == 'N') { this.direction = 'E'; }
+                        else if (this.direction == 'E') { this.direction = 'S'; }
+                        else if (this.direction == 'S') { this.direction = 'W'; }
+                        else if (this.direction == 'W') { this.direction = 'N'; }
+                        break;
+
+                    default:
+                        Console.WriteLine("\nInvalid Input");
+                        break;
+                }
+                i++;
+            }
+        }
+    }
+}
